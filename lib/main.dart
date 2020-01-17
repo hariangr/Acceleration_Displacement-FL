509541b1effd26aaa5c351ade94520ca7db8742d
Hari Anugrah <code.anugrah@gmail.com>
2020-01-17 20:42:10 +0800

    working sensor data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,6 +29,21 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      print(event);
+    });
+// [AccelerometerEvent (x: 0.0, y: 9.8, z: 0.0)]
+
+    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      print(event);
+    });
+// [UserAccelerometerEvent (x: 0.0, y: 0.0, z: 0.0)]
+
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      print(event);
+    });
+// [GyroscopeEvent (x: 0.0, y: 0.0, z: 0.0)]
+
     setState(() {
       _counter++;
     });
